4b390f690daabff8368be176a47ce483aee4522c
jx2hx1 <jx2hx1@gmail.com>
2022-12-05 20:32:40 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// const express = require('express')
+import express from "express";
+import imageToBase64 from "image-to-base64";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// GET 요청이 들어왔을 때
+app.get("/getBase64", async (req, res) => {
+  // res.send("Hello World!");
+  const { text } = req.query;
+
+  const temp = text.slice(
+    text.indexOf("http://www.nature.go.kr"),
+    text.indexOf("</imgUrl>")
+  );
+  // console.log(text);
+  const result = await imageToBase64(temp)
+    .then((response) => {
+      return response;
+      // console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return result;
+});
+
+app.listen(3000, () => {
+  // console.log(`Example app listening on port ${3000}`);
+}); // 3000번 포트
