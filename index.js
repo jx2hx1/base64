e87870975e827aba2f9ab0f3855cfac0d908b632
jx2hx1 <jx2hx1@gmail.com>
2022-12-07 19:07:36 +0900

    fix: add data:image/jpg;base64, to response

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const express = require('express')
 import express from "express";
 import imageToBase64 from "image-to-base64";
 import cors from "cors";
@@ -11,28 +10,20 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-// GET 요청이 들어왔을 때
 app.get("/getBase64", async (req, res) => {
-  // res.send("Hello World!");
   const { text } = req.query;
 
-  const temp = text.slice(
-    text.indexOf("http://www.nature.go.kr"),
-    text.indexOf("</imgUrl>")
-  );
-  // console.log(text);
-  const result = await imageToBase64(temp)
+  const result = await imageToBase64(text)
     .then((response) => {
-      return response;
+      return "data:image/jpg;base64," + response;
       // console.log(response);
     })
     .catch((error) => {
       console.log(error);
     });
-  // return result;
   res.send(result);
 });
 
 app.listen(3000, () => {
   // console.log(`Example app listening on port ${3000}`);
-}); // 3000번 포트
+});
